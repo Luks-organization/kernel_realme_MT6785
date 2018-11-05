f60e25e84c4d8aa20fd52e8dcb391dbbb2f7474c
Lucas Stach <l.stach@pengutronix.de>
2018-11-05 18:12:39 +0100

    drm/etnaviv: consolidate hardware fence handling in etnaviv_gpu
    [ Upstream commit 3283ee771c88bdf28d427b7ff0831a13213a812c ]
    
    This is the only place in the driver that should have to deal with
    the raw hardware fences. To avoid any further confusion, consolidate
    the fence handling in this file and remove any traces of this from
    the header files.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
    Stable-dep-of: 37dc4737447a ("drm/etnaviv: hold GPU lock across perfmon sampling")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/etnaviv/etnaviv_gpu.h
+++ drivers/gpu/drm/etnaviv/etnaviv_gpu.h
@@ -167,16 +167,6 @@ static inline u32 gpu_read(struct etnaviv_gpu *gpu, u32 reg)
 	return etnaviv_readl(gpu->mmio + reg);
 }
 
-static inline bool fence_completed(struct etnaviv_gpu *gpu, u32 fence)
-{
-	return fence_after_eq(gpu->completed_fence, fence);
-}
-
-static inline bool fence_retired(struct etnaviv_gpu *gpu, u32 fence)
-{
-	return fence_after_eq(gpu->retired_fence, fence);
-}
-
 int etnaviv_gpu_get_param(struct etnaviv_gpu *gpu, u32 param, u64 *value);
 
 int etnaviv_gpu_init(struct etnaviv_gpu *gpu);
