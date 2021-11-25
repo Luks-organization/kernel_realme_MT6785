a2ef112b85e0152e70de21dbd96e48a9e23fd515
Vaisakh Murali <mvaisakh@statixos.com>
2021-11-26 01:42:25 +0600

    drivers: misc/mtk: pmic: pmu: Avoid dumping registers
    This is unneeded on production builds
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- drivers/watchdog/mediatek/wdk/wd_common_drv.c
+++ drivers/watchdog/mediatek/wdk/wd_common_drv.c
@@ -572,6 +572,14 @@ static void kwdt_process_kick(int local_bit, int cpu,
 	 * [wdt-k]: kick watchdog actaully, this log is more important thus
 	 *	    using printk_deferred to ensure being printed.
 	 */
+	if (msg_buf[5] != 'k')
+		pr_info("%s", msg_buf);
+	else
+		printk_deferred("%s", msg_buf);
+#ifdef CONFIG_MTK_TICK_BROADCAST_AEE_DUMP
+	if (dump_timeout)
+		dump_wdk_bind_info();
+#endif
 
 #ifdef CONFIG_LOCAL_WDT
 	printk_deferred("[wdk] cpu:%d, kick local wdt,RT[%lld]\n",
