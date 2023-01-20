af8be37ca66a0496b8d03c94cc5d7a0be0586e8a
siimsek <malisimsek17@gmail.com>
2023-01-20 05:32:54 -0500

    printk: Silence logd logspam
    [  123.634939] logd: logdr: UID=1000 GID=1000 PID=9198 n tail=500 logMask=1d pid=0 start=0ns deadline=0ns
    
    Signed-off-by: siimsek <malisimsek17@gmail.com>
    Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1005,10 +1005,6 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
-			/* Only allow init: messages in the dmesg */
-			if (strncmp(line, "init:", strlen("init:")))
-				goto free;			
-
 			if (strstr(line, "logd"))
 				return ret;
 		}
