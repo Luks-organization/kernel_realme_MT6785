7dfeec2cfa9a395869efbf3029eae29c6c169795
Stim <stimluks87@gmail.com>
2025-05-28 18:19:49 +0500

    arm64: crypto: Add NEON accelerated XOR implementation. This is a NEON acceleration method that can improve performance by approximately 20%. I got the following data from the centos 7.5 on Huawei's HISI1616 chip:
    [ 93.837726] xor: measuring software checksum speed
    [ 93.874039]   8regs  : 7123.200 MB/sec
    [ 93.914038]   32regs : 7180.300 MB/sec
    [ 93.954043]   arm64_neon: 9856.000 MB/sec
    [ 93.954047] xor: using function: arm64_neon (9856.000 MB/sec)
    
    I believe this code can bring some optimization for
    all ARM64 platform. thanks for Ard Biesheuvel's suggestions.

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ echo -e " Kernel Compile Successful"
 git clone --depth=1 https://github.com/Luks-organization/AnyKernel3 AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 Psionic-R3-Salaa-Kernel-${TANGGAL}.zip *
+zip -r9 Lineage-Salaa-Kernel-${TANGGAL}.zip *
 cd ../
 make clean && make mrproper
 fi
