25fcbd4ad0cf875f3e2b61be9cd005b1c2bde76c
StimLuks87 <san10031987san@gmail.com>
2025-08-25 14:41:18 +0500

    BACKPORT: maccess: rename strncpy_from_unsafe_user to strncpy_from_user_nofault This matches the naming of strncpy_from_user, and also makes it more clear what the function is supposed to do.
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20200521152301.2587579-7-hch@lst.de
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    cherry picked from torvalds/linux@bd88bb5 upstream
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- mm/maccess.c
+++ mm/maccess.c
@@ -181,8 +181,8 @@ long strncpy_from_unsafe(char *dst, const void *unsafe_addr, long count)
 }
 
 /**
- * strncpy_from_unsafe_user: - Copy a NUL terminated string from unsafe user
- *				address.
+ * strncpy_from_user_nofault: - Copy a NUL terminated string from unsafe user
+ *		address.
  * @dst:   Destination address, in kernel space.  This buffer must be at
  *         least @count bytes long.
  * @unsafe_addr: Unsafe user address.
@@ -198,8 +198,8 @@ long strncpy_from_unsafe(char *dst, const void *unsafe_addr, long count)
  * If @count is smaller than the length of the string, copies @count-1 bytes,
  * sets the last byte of @dst buffer to NUL and returns @count.
  */
-long strncpy_from_unsafe_user(char *dst, const void __user *unsafe_addr,
-			      long count)
+long strncpy_from_user_nofault(char *dst, const void __user *unsafe_addr,
+	          long count)
 {
 	mm_segment_t old_fs = get_fs();
 	long ret;
