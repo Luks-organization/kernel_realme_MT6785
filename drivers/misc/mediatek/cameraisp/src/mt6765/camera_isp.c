ea1768958fb822626e3f4f4bd70c38460169712d
StimLuks87 <san10031987san@gmail.com>
2025-09-02 03:06:04 +0500

    Revert "[ALPS07436563] P1: Fix unhandled case in ISP_WriteReg"
    This reverts commit 8e8390294036af10cd6f702718e3d880cc9388bf.

--- drivers/misc/mediatek/cameraisp/src/mt6765/camera_isp.c
+++ drivers/misc/mediatek/cameraisp/src/mt6765/camera_isp.c
@@ -4284,9 +4284,8 @@ static signed int ISP_WriteReg(struct ISP_REG_IO_STRUCT *pRegIo)
 	/* unsigned char* pData = NULL; */
 	struct ISP_REG_STRUCT *pData = NULL;
 
-	if (((pRegIo->Count * sizeof(struct ISP_REG_STRUCT)) > 0xFFFFF000) ||
-		(pRegIo->Count == 0)) {
-		pr_err("pRegIo->Count error");
+	if ((pRegIo->Count * sizeof(struct ISP_REG_STRUCT)) > 0xFFFFF000) {
+		LOG_NOTICE("pRegIo->Count error");
 		Ret = -EFAULT;
 		goto EXIT;
 	}
--- drivers/misc/mediatek/cameraisp/src/mt6768/camera_isp.c
+++ drivers/misc/mediatek/cameraisp/src/mt6768/camera_isp.c
@@ -4537,8 +4537,7 @@ static signed int ISP_WriteReg(struct ISP_REG_IO_STRUCT *pRegIo)
 	/* unsigned char* pData = NULL; */
 	struct ISP_REG_STRUCT *pData = NULL;
 
-	if (((pRegIo->Count * sizeof(struct ISP_REG_STRUCT)) > 0xFFFFF000) ||
-		(pRegIo->Count == 0)) {
+	if ((pRegIo->Count * sizeof(struct ISP_REG_STRUCT)) > 0xFFFFF000) {
 		LOG_NOTICE("pRegIo->Count error");
 		Ret = -EFAULT;
 		goto EXIT;
