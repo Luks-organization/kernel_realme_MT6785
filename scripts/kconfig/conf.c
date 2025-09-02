b16d1d3d22e06e65a3f2fa6f7376f80eae2c8ddd
StimLuks87 <san10031987san@gmail.com>
2025-09-03 01:08:31 +0500

    Revert "kconfig: make xfgets() really static"
    This reverts commit 5a3dc717b3c785242e6d40f08288947cb6751ee4.

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -20,6 +20,7 @@
 
 static void conf(struct menu *menu);
 static void check_conf(struct menu *menu);
+static void xfgets(char *str, int size, FILE *in);
 
 enum input_mode {
 	oldaskconfig,
@@ -80,13 +81,6 @@ static void check_stdin(void)
 		printf(_("Run 'make oldconfig' to update configuration.\n\n"));
 		exit(1);
 	}
-}
-
-/* Helper function to facilitate fgets() by Jean Sacren. */
-static void xfgets(char *str, int size, FILE *in)
-{
-	if (!fgets(str, size, in))
-		fprintf(stderr, "\nError in reading or end of file.\n");
 }
 
 static int conf_askvalue(struct symbol *sym, const char *def)
@@ -718,4 +712,13 @@ int main(int ac, char **av)
 		}
 	}
 	return 0;
+}
+
+/*
+ * Helper function to facilitate fgets() by Jean Sacren.
+ */
+void xfgets(char *str, int size, FILE *in)
+{
+	if (fgets(str, size, in) == NULL)
+		fprintf(stderr, "\nError in reading or end of file.\n");
 }
