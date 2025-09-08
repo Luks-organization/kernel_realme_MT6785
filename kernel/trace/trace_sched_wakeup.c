18c9c1105d093b5de596c60a5efda5fd404f780f
StimLuks87 <san10031987san@gmail.com>
2025-09-09 02:14:26 +0500

    Revert "[TREEWIDE] Guard tracing functions with CONFIG_TRACING"
    This reverts commit 52e924635ee98cd984190b63a95bf874a32350cb.

--- kernel/trace/trace_sched_wakeup.c
+++ kernel/trace/trace_sched_wakeup.c
@@ -449,9 +449,7 @@ probe_wakeup_sched_switch(void *ignore, bool preempt,
 	int cpu;
 	int pc;
 
-    #ifdef CONFIG_TRACING
 	tracing_record_cmdline(prev);
-    #endif
 
 	if (unlikely(!tracer_enabled))
 		return;
@@ -546,10 +544,8 @@ probe_wakeup(void *ignore, struct task_struct *p)
 	if (likely(!tracer_enabled))
 		return;
 
-#ifdef CONFIG_TRACING
 	tracing_record_cmdline(p);
 	tracing_record_cmdline(current);
-#endif
 
 	/*
 	 * Semantic is like this:
